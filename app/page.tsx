df8bdf4fdc08f3a14a9d34942b6db5cc00dac223
ahmad-ml <ahmad@niftyitsolution.com>
2023-09-01 10:40:06 +0600

    Add sample response

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,20 @@
-import Image from 'next/image'
-import styles from './page.module.css'
+import Link from 'next/link';
+import styles from './page.module.css';
+
 
 export default function Home() {
   return (
     <main className={styles.main}>
-     
+     <div>
+        <h1>Ahmad Ostad assignment 13</h1>
+        <Link href={'/api/json'}>
+        <p>Here is my sample json response</p>
+        </Link>
+        <Link href={'/api/content'}>
+        <p>Here is my sample content response</p>
+        </Link>
+     </div>
     </main>
   )
 }
+
